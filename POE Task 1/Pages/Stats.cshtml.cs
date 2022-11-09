024a9cdbc709668e4acf118c61e71c89d49ff883
Bongumusa <83663904+BongumusaCele@users.noreply.github.com>
2022-11-09 10:42:19 +0200

    Headings added to the information page

--- POE Task 1/Pages/Stats.cshtml.cs
+++ POE Task 1/Pages/Stats.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Data.SqlClient;
 
 namespace POE_Task_1.Pages
 {
     public class StatsModel : PageModel
     {
+        public decimal availableMoney;
         public void OnGet()
         {
+            try
+            {
+                string connectionString = "Data Source=LAPTOP-EJ02DD7T\\SQLEXPRESS;Initial Catalog=CLIENTS;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlMonetary = "SELECT SUM(Amount) FROM MonetaryDonations";
+
+                    using (SqlCommand command = new SqlCommand(sqlMonetary, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                availableMoney = reader.GetDecimal(0);
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
     }
 }
