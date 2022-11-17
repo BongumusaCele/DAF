0a5a1db1a39f165bb9852592b1a5e25275bc23f6
Bongumusa <83663904+BongumusaCele@users.noreply.github.com>
2022-11-17 16:22:09 +0200

    Test classes added

---
+++ UnitTests_Tests/TestDisaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POE_Task_1.Pages;
+
+namespace UnitTests_Tests
+{
+    internal class TestDisaster
+    {
+    }
+}
